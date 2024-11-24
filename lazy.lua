af507124ab20efd89ab1dc3c6ea4ddbfc8c843b3
Shrish <shrish.missioniisc@gmail.com>
2024-11-24 21:00:59 +0530

    Add files via upload

--- lazy.lua
+++ lazy.lua
@@ -15,7 +15,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("shrish.plugins", {
+require("lazy").setup({{import = "shrish.plugins"}, {import = "shrish.plugins.lua"}}, {
   checker = {
     enabled = true,
     notify = false,
